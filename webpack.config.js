0c3459c9e5a804416fb7494e7442101b728c627d
marcos <marcodamasceno07@gmail.com>
2022-05-25 18:08:18 -0300

    :heavy_plus_sign: added webpack webpack-cli css-loader mini-css-extract-plugin

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+// const miniCssExtractPlugin = requer("mini-css-extrato-plugin")
+// const path = require("path")
+
+// módulo.exportações = {
+//     entrada: './src/index.js ',
+//     saída: {
+//         path: path.resolver(__dirname , './ dist '),
+//         filename: 'bundle.js'
+//     },
+//     modo : 'desenvolvimento',
+//     módulo : {
+//         regras:[
+//             {
+                
+//             teste: /\.css$/,
+//             usar: [miniCssExtractPlugin.carregador ,'css-loader']
+            
+//             }
+//         ]
+//     },
+//     plugins : [
+//         novo miniCssExtractPlugin({
+//             nome do arquivo: 'style.css'
+//         })
+//     ]
+// }
